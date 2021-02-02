5574f7ee36240d744fee8e08d59529e1826a8deb
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-02-01 18:39:39 -0600

    example cin, get total w test case

--- test/examples_test/01_module_test/01_module_tests.cpp
+++ test/examples_test/01_module_test/01_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "vars.h"
+#include "input.h"
 #include<string>
 
 TEST_CASE("Verify Test Configuration", "verification") {
@@ -13,6 +14,11 @@ TEST_CASE("Verify Echo Variable", "verify function param is returned") {
 	REQUIRE(echo_variable(10) == 10);
 }
 
+TEST_CASE("Verify get total function", "returns product of units * rate") 
+{
+	REQUIRE(get_total(5, 10) == 50);
+	REQUIRE(get_total(6, 6) == 36);
+}
 
 /*test case add_to_double_1 with 0 as parameter*/
 
