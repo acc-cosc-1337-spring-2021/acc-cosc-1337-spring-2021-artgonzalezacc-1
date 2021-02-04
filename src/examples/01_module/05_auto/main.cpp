3cf95e42fd5355041de8321b065c61ab7350692b
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-02-03 18:41:07 -0600

    Example: int, auto, char, string, doubles

--- src/examples/01_module/05_auto/main.cpp
+++ src/examples/01_module/05_auto/main.cpp
@@ -1,8 +1,12 @@
+#include "auto.h"
+#include <iostream>
 
+using std::cout;
 
 int main() 
 {
-		
+	auto num = 10;
+
 
 	return 0;
 }
--- test/examples_test/01_module_test/01_module_tests.cpp
+++ test/examples_test/01_module_test/01_module_tests.cpp
@@ -2,6 +2,9 @@
 #include "catch.hpp"
 #include "vars.h"
 #include "input.h"
+#include "numbers.h"
+#include "auto.h"
+#include "chars.h"
 #include<string>
 
 TEST_CASE("Verify Test Configuration", "verification") {
@@ -21,14 +24,27 @@ TEST_CASE("Verify get total function", "returns product of units * rate")
 }
 
 /*test case add_to_double_1 with 0 as parameter*/
-
 /*test case add_to_double_1 with 1 as parameter*/
+TEST_CASE("Verify add to double 1 function", "verify decimal comparison inconsistencies") 
+{
+	//REQUIRE(add_to_double_1(0) == .9);
+	REQUIRE(add_to_double_1(1) == 1.9);
+}
 
 /*test case add_to_double_2 with 0 as parameter*/
 
 /*test case add_to_double_2 with 1 as parameter*/
+TEST_CASE("Verify auto int function", "should return 10") 
+{
+	REQUIRE(auto_int() == 10);
+}
 
 /*test case char test ASCII values */
 
 /*test case string test ASCII values Joe characters are equal to 74, 111, and 101*/
-
+TEST_CASE("Verify get char ascii function", "should return ascii value") 
+{
+	REQUIRE(get_char_ascii('J') == 74);
+	REQUIRE(get_char_ascii('o') == 111);
+	REQUIRE(get_char_ascii('e') == 101);
+}
