5e1cb31a5919d15e665cc9a48690c2da998ad701
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-03-23 19:57:53 -0500

    vector ref, value, and init examples

--- src/examples/04_module/06_vectors/main.cpp
+++ src/examples/04_module/06_vectors/main.cpp
@@ -1,7 +1,23 @@
 #include "vec.h"
+#include <string>
+using std::string;
+
+using std::vector;
+using std::cout;
 
 int main() 
 {
+	vector<string> names(9, "joe");
+	names.insert(names.begin(), "mary");
+		
+	for(auto name: names)
+	{
+		cout<<name<<"\n";
+	}
+	
+	
+	
+
 
 	return 0;
 }
