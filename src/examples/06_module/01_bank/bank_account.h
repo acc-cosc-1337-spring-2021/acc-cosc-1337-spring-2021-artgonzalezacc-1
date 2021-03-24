3c86ba19145cc592d9d58da9f2f7b4f7f37e5834
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-03-23 19:58:18 -0500

    bank account class init

--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -1,1 +1,11 @@
 //bank_account.h
+class BankAccount
+{
+public://access specifiers-USERS OF OUR CLASS(OTHER OBJECTS) CAN SEE WHAT'S IN PUBLIC
+    int get_balance()const{return balance;}
+    void deposit(int amount);
+    void withdraw(int amount);
+   
+private://access specifier-ONLY BANK ACCOUNT CLASS SEES DATA/FUNCTIONS IN PRIVATE
+    int balance{0};//protect the  data integrity 
+};
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,6 +1,27 @@
+#include "bank_account.h"
+#include<iostream>
+using std::cout;
+
 
 int main()
 {
+	BankAccount account;
+	
+//	cout<<sizeof(account)<<"\n";
+	auto balance = account.get_balance();
+
+	cout<<"Balance: "<<balance<<"\n";
+
+	cout<<"Deposit $50\n";
+	account.deposit(50);
+
+	balance = account.get_balance();
+	cout<<"Balance: "<<balance<<"\n\n";
+
+	cout<<"Withdraw $10\n";
+	account.withdraw(10);
+	balance = account.get_balance();
+	cout<<"Balance: "<<balance<<"\n";
 
 	return 0;
 }
