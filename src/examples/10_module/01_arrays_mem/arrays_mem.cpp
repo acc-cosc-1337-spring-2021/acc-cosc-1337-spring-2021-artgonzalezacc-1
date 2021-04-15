216e1d7a190b3119d02f602def4a1ce83242c977
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-04-15 18:43:30 -0500

    Stack array basics

--- src/examples/10_module/01_arrays_mem/arrays_mem.cpp
+++ src/examples/10_module/01_arrays_mem/arrays_mem.cpp
@@ -1,1 +1,44 @@
 //write code for for stack_array and display each element to screen
+#include "arrays_mem.h"
+
+using std::cout;
+
+void use_stack_array()
+{
+    const int SIZE = 5;
+    int number[SIZE]{1, 2};//RESERVE five stack slots of 4 bytes each
+
+    for(int i=0; i < SIZE; ++i)
+    {
+        //number[i] = i + 1;
+    }
+
+    for(int i=0; i < SIZE; ++i)
+    {
+        cout<<number[i]<<"\n";
+    }
+}
+
+void array_months()
+{
+    const int NO_OF_MONTHS = 3;
+
+    int months [NO_OF_MONTHS]{1, 2, 3};
+    std::string month_names[NO_OF_MONTHS]{"Jan", "Feb", "Mar"};
+
+    for(int i=0; i < NO_OF_MONTHS; ++i) 
+    {
+        cout<<months[i]<<" "<<month_names[i]<<"\n";
+    }
+}
+
+void array_for_ranged()
+{
+    const int SIZE = 5;
+    int numbers[SIZE]{1, 2};    
+
+    for(auto number: numbers)
+    {
+        cout<<number<<"\n";
+    }
+}
--- src/examples/10_module/01_arrays_mem/main.cpp
+++ src/examples/10_module/01_arrays_mem/main.cpp
@@ -1,9 +1,10 @@
-
+#include "arrays_mem.h"
 
 int main() 
 {
-
-	
+	//use_stack_array();
+	//array_months();
+	array_for_ranged();
 
 	return 0;
 
