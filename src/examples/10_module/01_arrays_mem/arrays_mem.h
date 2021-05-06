d181e35624c01b704fd373416ce943c26f7e5f0d
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2021-05-06 17:21:06 -0500

    Add files via upload

--- src/examples/10_module/01_arrays_mem/arrays_mem.h
+++ src/examples/10_module/01_arrays_mem/arrays_mem.h
@@ -1,6 +1,9 @@
 //arrays_mem.h - stack array example
 #include<iostream>
 #include<string>
+#include <ctime>
+#include <cstdlib>
+#include <chrono>
 
 //function prototype for stack_array
 void use_stack_array();
@@ -8,3 +11,11 @@ void use_stack_array();
 void array_months();
 
 void array_for_ranged();
+
+void populate_array(int list[], int size, bool randomize);
+
+int linear_search(const int list[], int size, int search_value);
+
+int binary_search(const int list[], int size, int search_value);
+
+void bubble_sort(int list[], int size);
--- src/examples/10_module/01_arrays_mem/main.cpp
+++ src/examples/10_module/01_arrays_mem/main.cpp
@@ -1,11 +1,27 @@
 #include "arrays_mem.h"
 
-int main() 
-{
-	//use_stack_array();
-	//array_months();
-	array_for_ranged();
+int main ()
+{	
 
-	return 0;
+  const int SIZE = 10000;
+  int* list = new int[SIZE];
+  populate_array(list, SIZE, false);
 
-}
+  std::cout<<"function executing...: \n";
+  std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+  //500000,1000000
+  //linear_search(list, SIZE, -5);
+  //500000,1000000
+  binary_search(list, SIZE, -5);
+  //10000,100000,200000
+  //bubble_sort(list, SIZE);
+  std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+  std::cout<<"function done: \n";
+
+  std::cout<<"Milliseconds: "<<std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
+
+  delete list;
+
+  return 0;
+} 	
